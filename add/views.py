61ed2f7f30153a465874a71276decc9d6d3805f4
Gonçalo Sousa <gsousa92@gmail.com>
2018-01-23 21:13:39 +0000

    'Challenge2'

--- add/views.py
+++ add/views.py
@@ -3,19 +3,15 @@ from django.http import HttpResponseRedirect
 
 from django.contrib import messages
 
-
-
 from .models import User
-
-from add.models import User
-from add.forms import UserForm
+from .forms import UserForm
 
 def AddPage(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            name = request.POST.get('name', '')
-            email = request.POST.get('email', '')
+            name = request.POST.get('name', 'Wrong Name/Email format, try again please')
+            email = request.POST.get('email',  'Wrong Name/Email format, try again please')
             user = User(name = name, email = email)
             user.save()
 
